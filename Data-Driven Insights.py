678074c88a95210ef165bf9596da952682ca3199
Tribhuvan singh <tribhuvanrana10@gmail.com>
2026-01-19 21:28:41 +0530

    Update Data-Driven Insights.py

--- Data-Driven Insights.py
+++ Data-Driven Insights.py
@@ -1,12 +1,11 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Example: Visualizing loan approval trends based on applicants' income
 sns.boxplot(x='Loan_Status', y='Income', data=data)
 plt.title('Loan Approval by Income')
 plt.show()
 
-# Example: Visualizing loan approval trends based on credit score
+
 sns.scatterplot(x='Credit_Score', y='Loan_Status', data=data)
 plt.title('Loan Approval by Credit Score')
 plt.show()
